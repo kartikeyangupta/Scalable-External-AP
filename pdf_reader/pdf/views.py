20cdc270615cf62225e62fe6ff3ebb4fbcb806c6
Kartikeyan  Gupta <kartikeyangupta@Kartikeyans-MacBook-Air.local>
2024-02-06 22:58:29 +0530

    Fix(): Error handling for content API

--- pdf_reader/pdf/views.py
+++ pdf_reader/pdf/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render, get_list_or_404
 from .models import PDF_File, File_Content
 from .forms import PDFFileForm
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from .tasks import upload_pdf_to_s3
 
 def index(request):
@@ -29,7 +30,18 @@ def uploadpdf(request):
     return render(request, "pdf/upload.html", {"form": form})
 
 def view_pdf_content(request, file_id):
-    pdf_file = get_object_or_404(File_Content, file_name_id=file_id)
-    file_name = PDF_File.objects.get(pk=pdf_file.file_name_id).name
-    return render(request, "pdf/content.html", {'content': pdf_file.content, 'name': file_name})
+    try:
+        file_name = None
+        file_name = PDF_File.objects.get(pk=file_id)
+        pdf_file = File_Content.objects.get(file_name_id=file_id)
+        return render(request, "pdf/content.html", {'content': pdf_file.content, 'name': file_name})
+    except ObjectDoesNotExist:
+        if file_name:
+            msg = "Lets give it retry after some time, your file is being processed by our servers."
+            return render(request, "pdf/content.html", {'content': msg, 'name': file_name})
+        else:
+            msg = f'The File id {file_id} doesnt exist on our server.'
+            return render(request, "pdf/content.html", {'content': msg, 'name': 'Incorrect PDF File ID'})
     
+       
+
